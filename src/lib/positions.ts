6df6b349c0e07eb018ca165d4e9e33560e537def
caboharry <fundedbyharry@gmail.com>
2026-03-18 17:47:00 -0500

    Create positions.ts with position data

---
+++ src/lib/positions.ts
@@ -0,0 +1,8 @@
+// Position data
+const positions = [
+    { name: 'Position 1', value: 100 },
+    { name: 'Position 2', value: 200 },
+    { name: 'Position 3', value: 300 }
+];
+
+export default positions;
